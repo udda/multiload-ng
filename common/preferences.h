f3b0232db61fd1671c7a5ca53ecd04d4c36cb55b
Mario Cianciolo <mr.udda@gmail.com>
2016-09-14 14:48:28 +0200

    Lower default scaler max for Load graph Better display for dual and single cores

--- common/preferences.h
+++ common/preferences.h
@@ -91,7 +91,7 @@ enum MultiloadDefaults {
 	DEFAULT_TOOLTIP_STYLE				= TOOLTIP_STYLE_SIMPLE,
 
 	DEFAULT_MAX_VALUE					= -1,
-	DEFAULT_MAX_VALUE_LOAD				= 10,		// should cover well single to quad cores
+	DEFAULT_MAX_VALUE_LOAD				= 8,		// should cover well single to quad cores
 	DEFAULT_MAX_VALUE_TEMP				= 120000	// measured in millidegrees
 };
 #define DEFAULT_COLOR_SCHEME "Multiload-ng"
