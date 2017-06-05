4e0543efed0e331e47982d2d889affb4fa32ec4d
Mario Cianciolo <mr.udda@gmail.com>
2017-06-05 17:28:01 +0200

    Properly handle zero swap case

--- libmultiload/src/provider/swap.c
+++ libmultiload/src/provider/swap.c
@@ -2,17 +2,17 @@
  * Copyright (C) 2017 Mario Cianciolo <mr.udda@gmail.com>
  *
  * This file is part of Multiload-ng.
- * 
+ *
  * Multiload-ng is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Multiload-ng is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Multiload-ng.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -37,7 +37,7 @@ const MlGraphTypeInterface ML_PROVIDER_SWAP_IFACE = {
 	.sizeof_fn		= NULL,
 	.unpause_fn		= NULL,
 	.caption_fn		= ml_provider_swap_caption,
-	
+
 	.helptext		= NULL
 };
 
@@ -75,8 +75,11 @@ ml_provider_swap_caption (MlCaption *caption, MlDataset *ds, ML_UNUSED mlPointer
 	ml_string_format_size_s ((uint64_t)kb_used  * 1024, "B", false, buf_used,  sizeof (buf_used));
 	ml_string_format_size_s ((uint64_t)kb_total * 1024, "B", false, buf_total, sizeof (buf_total));
 
-	ml_caption_set (caption, ML_CAPTION_COMPONENT_HEADER, _("Total swap: %s"), buf_total);
-
-	// TRANSLATORS: used swap, absolute and percentage value
-	ml_caption_set (caption, ML_CAPTION_COMPONENT_BODY, _("%s (%0.01f%%) used swap"), buf_used, (100.0 * kb_used / kb_total));
+	if (kb_total > 0) {
+		ml_caption_set (caption, ML_CAPTION_COMPONENT_HEADER, _("Total swap: %s"), buf_total);
+		// TRANSLATORS: used swap, absolute and percentage value
+		ml_caption_set (caption, ML_CAPTION_COMPONENT_BODY, _("%s (%0.01f%%) used swap"), buf_used, (100.0 * kb_used / kb_total));
+	} else {
+		ml_caption_set (caption, ML_CAPTION_COMPONENT_BODY, _("No swap"));
+	}
 }
